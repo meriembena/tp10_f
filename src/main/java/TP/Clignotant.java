a8925a544c4f557fb20a8f80041ab5bc96386582
John Doe <johndoe@example.com>
2024-12-06 21:28:16 +0100

    modification

---
+++ src/main/java/TP/Clignotant.java
@@ -0,0 +1,24 @@
+package TP;
+import bandeau.Bandeau;
+import java.awt.Color;
+
+public class Clignotant extends Effet {
+    private int repetitions;
+    private int delay;
+
+    public Clignotant(Bandeau bandeau, int repetitions, int delay) {
+        super(bandeau);
+        this.repetitions = repetitions;
+        this.delay = delay;
+    }
+
+    @Override
+    public void jouer() {
+        for (int i = 0; i < repetitions; i++) {
+            bandeau.setForeground(Color.WHITE);
+            bandeau.sleep(delay);
+            bandeau.setForeground(Color.BLACK);
+            bandeau.sleep(delay);
+        }
+    }
+}
---
+++ src/main/java/TP/Zoom.java
@@ -0,0 +1,28 @@
+package TP;
+import bandeau.Bandeau;
+import java.awt.Font;
+
+public class Zoom extends Effet {
+    private int steps;
+    private int delay;
+
+    public Zoom(Bandeau bandeau, int steps, int delay) {
+        super(bandeau);
+        this.steps = steps;
+        this.delay = delay;
+    }
+
+    @Override
+    public void jouer() {
+        Font originalFont = bandeau.getFont();
+        int originalSize = originalFont.getSize();
+
+        for (int i = 0; i < steps; i++) {
+            bandeau.setFont(new Font(originalFont.getName(), originalFont.getStyle(), originalSize + i));
+            bandeau.sleep(delay);
+        }
+
+        // Restaure la taille d'origine
+        bandeau.setFont(originalFont);
+    }
+}
